fe8ee94ac448c7ed25bbffdfc9ef5a5aa9fe0c77
Daybreak <pablo999picon@gmail.com>
2025-02-05 14:20:57 +0100

    Add initial component structure and user data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoAngular0001';
+  nombre:string = 'Pablo';
+  fumador:boolean = false;
+  notas = [8.1, 5.6, 3.2];
+  //notas:array = [8.1, 5.6, 3.2];
+
+  esfumador(){
+    return this.fumador? 'Sí es fumador' : 'No es fumador';
+  }
 }
