acce22fcaeb87506693f0153aa249f9b670dd433
Daybreak <pablo999picon@gmail.com>
2025-02-06 13:15:33 +0100

    Shit i do day 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,36 @@ export class AppComponent {
   fumador:boolean = false;
   notas = [8.1, 5.6, 3.2];
   //notas:array = [8.1, 5.6, 3.2];
+  edad:number = 19;
+  counter:number  =0;
 
   esfumador(){
     return this.fumador? 'Sí es fumador' : 'No es fumador';
   }
+
+  provincias = [
+
+    {nombre: 'Almería', capital: 'Almería'},
+  {nombre: 'Cádiz', capital: 'Cádiz'},
+  {nombre: 'Córdoba', capital: 'Córdoba'},
+  {nombre: 'Granada', capital: 'Granada'},
+  {nombre: 'Huelva', capital: 'Huelva'},
+  {nombre: 'Jaén', capital: 'Jaén'},
+  ];
+
+  pulsarBoton(){
+    alert('Has pulsado el botón');
+  }
+
+  incrementar(){
+    this.counter++;
+  }
+
+  decrementar(){
+    if(this.counter>0){
+      
+      this.counter--;
+    }
+  }
+
 }
